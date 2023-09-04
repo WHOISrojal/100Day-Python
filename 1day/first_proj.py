d66a2db4b1b0780e57008641de38560cca76af29
WHOISrojal <rojal2002@gmail.com>
2023-09-04 14:50:01 +0545

    First Day Python

---
+++ 1day/first_proj.py
@@ -0,0 +1,5 @@
+# To make a simple program using variables, input and concat function.
+print("Hello!, Welcome to your program\n")
+city = input("Enter the city you grew up\n")
+pet = input("Enter your pet name\n")
+print("The brand name could be\n"+city+pet)
