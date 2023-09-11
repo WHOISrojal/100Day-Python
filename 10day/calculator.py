223d9bfed453a1030162ffaae428261585210249
WHOISrojal <rojal2002@gmail.com>
2023-09-11 21:33:27 +0545

    10th day python end

---
+++ 10day/calculator.py
@@ -0,0 +1,36 @@
+from art import logo
+def add(n1, n2):
+    return n1 + n2
+
+def sub(n1, n2):
+    return n1 - n2
+
+def mul(n1, n2):
+    return n1 * n2
+
+def div(n1, n2):
+    return n1 / n2
+
+operations = {"+": add, "-": sub, "*": mul, "/": div}
+
+def calculator():
+    print(logo)
+    num1 = float(input("Enter first number:"))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
+
+    while should_continue:
+        operation_symbol = input("Pick an operation symbol:")
+        num2 = float(input("Enter second number:"))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Enter 'y' to continue calculating with {answer}, or type 'n' to start a new calculation:") == "y":
+            num1 = answer
+        else:
+            should_continue  = False
+            calculator()
+calculator()
